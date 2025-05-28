784f483c8b7a9fff63d522d8715ed83778ac9c21
Ikuya Nagaki <ikuya313198@gmail.com>
2025-05-28 12:15:17 +0900

    generalize inheritance tree building

---
+++ refdiff-universal/src/main/java/refdiff/parsers/universal/common/InheritanceTreeGenerator.java
@@ -0,0 +1,104 @@
+package refdiff.parsers.universal.common;
+
+import org.treesitter.TSLanguage;
+import org.treesitter.TSTree;
+import org.treesitter.TSNode;
+import org.treesitter.TSQuery;
+import org.treesitter.TSQueryCapture;
+import org.treesitter.TSQueryCursor;
+import org.treesitter.TSQueryMatch;
+
+import refdiff.core.cst.CstNode;
+import refdiff.core.cst.CstRoot;
+import refdiff.core.cst.CstNodeRelationship;
+import refdiff.core.cst.CstNodeRelationshipType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class InheritanceTreeGenerator {
+
+    private final List<String> participatingNodeTypes;
+    private final String inheritanceConstructQueryString;
+
+    /**
+     * Constructs an InheritanceTreeGenerator.
+     * @param typeDefiningNodeTypes A list of CST node type strings that represent entities participating in inheritance (e.g., "CLASS_DECLARATION", "INTERFACE_DECLARATION").
+     * @param inheritanceConstructQueryString The Tree-sitter query string to find nodes that define inheritance relationships (e.g., superclass clauses, implements clauses).
+     */
+    public InheritanceTreeGenerator(List<String> typeDefiningNodeTypes, String inheritanceConstructQueryString) {
+        this.participatingNodeTypes = new ArrayList<>(typeDefiningNodeTypes); // Defensive copy
+        this.inheritanceConstructQueryString = inheritanceConstructQueryString;
+    }
+
+    public void buildInheritanceTree(CstRoot root, Map<String, String> sourceCodeMap, Map<String, TSTree> parsedTreeMap, TSLanguage tsLang) {
+        List<CstNode> participatingTypeNodes = new ArrayList<>();
+        for (CstNode topLevelNode : root.getNodes()) {
+            collectParticipatingTypeNodesRecursively(topLevelNode, participatingTypeNodes);
+        }
+        Map<String, CstNode> participatingTypeNodeMap = new HashMap<>();
+        for (CstNode participatingTypeNode : participatingTypeNodes) {
+            participatingTypeNodeMap.put(participatingTypeNode.getSimpleName(), participatingTypeNode); // TODO: Using simplename for the hashmap key, so duplicates can easily overwrite. Using fully qualified names or similar might be better.
+        }
+
+        for (Map.Entry<String, TSTree> treeEntry : parsedTreeMap.entrySet()) {
+            TSTree tree = treeEntry.getValue();
+            String filePath = treeEntry.getKey();
+            String sourceCode = sourceCodeMap.get(filePath);
+            addInheritanceRelationshipsForFile(root, participatingTypeNodeMap, tree, tsLang, sourceCode);
+        }
+    }
+
+    private void collectParticipatingTypeNodesRecursively(CstNode node, List<CstNode> collectedNodes) {
+        String nodeType = node.getType();
+        if (this.participatingNodeTypes.contains(nodeType)) {
+            collectedNodes.add(node);
+        }
+        for (CstNode child : node.getNodes()) {
+            collectParticipatingTypeNodesRecursively(child, collectedNodes);
+        }
+    }
+
+    private void addInheritanceRelationshipsForFile(CstRoot root, Map<String, CstNode> participatingTypeNodeMap, TSTree tree, TSLanguage tsLang, String sourceCode) {
+        TSQuery tsQuery = new TSQuery(tsLang, this.inheritanceConstructQueryString);
+        TSQueryCursor cursor = new TSQueryCursor();
+        TSNode rootNode = tree.getRootNode();
+        cursor.exec(tsQuery, rootNode);
+        TSQueryMatch match = new TSQueryMatch();
+
+        while (cursor.nextMatch(match)) {
+            for (TSQueryCapture capture : match.getCaptures()) {
+                TSNode tsNode = capture.getNode();
+                switch (tsNode.getType()) {
+                    case "superclass": { // e.g., class A extends B
+                        TSNode superclass = tsNode;
+                        TSNode extendsToken = superclass.getChild(0);
+                        String superclassName = sourceCode.substring(extendsToken.getEndByte(), superclass.getEndByte()).trim();
+                        TSNode identifier = superclass.getParent().getChildByFieldName("name");
+                        String className = sourceCode.substring(identifier.getStartByte(), identifier.getEndByte());
+                        if (participatingTypeNodeMap.containsKey(className) && participatingTypeNodeMap.containsKey(superclassName)) {
+                            root.getRelationships().add(new CstNodeRelationship(CstNodeRelationshipType.SUBTYPE, participatingTypeNodeMap.get(className).getId(), participatingTypeNodeMap.get(superclassName).getId()));
+                        }
+                        break;
+                    }
+                    case "super_interfaces":   // e.g., class A implements B, C
+                    case "extends_interfaces": { // e.g., interface A extends B, C
+                        TSNode interfacesNode = tsNode;
+                        TSNode subTypeNameNode = interfacesNode.getParent().getChildByFieldName("name");
+                        String subTypeName = sourceCode.substring(subTypeNameNode.getStartByte(), subTypeNameNode.getEndByte());
+                        TSNode firstTokenAfterKeyword = interfacesNode.getChild(0); // e.g., 'implements' or 'extends' token
+                        List<String> superTypeNames = java.util.Arrays.stream(sourceCode.substring(firstTokenAfterKeyword.getEndByte(), interfacesNode.getEndByte()).split(",")).map(String::trim).toList();
+                        for (String name : superTypeNames) {
+                            if (participatingTypeNodeMap.containsKey(subTypeName) && participatingTypeNodeMap.containsKey(name)) {
+                                root.getRelationships().add(new CstNodeRelationship(CstNodeRelationshipType.SUBTYPE, participatingTypeNodeMap.get(subTypeName).getId(), participatingTypeNodeMap.get(name).getId()));
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
--- refdiff-universal/src/main/java/refdiff/parsers/universal/java/JavaParser.java
+++ refdiff-universal/src/main/java/refdiff/parsers/universal/java/JavaParser.java
@@ -10,9 +10,9 @@ import org.treesitter.TSQueryCapture;
 import org.treesitter.TSQueryCursor;
 import org.treesitter.TSQueryMatch;
 
-import java.util.List;
 import java.util.Map;
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 import java.util.HashMap;
 
 
@@ -20,13 +20,12 @@ import refdiff.core.io.SourceFileSet;
 import refdiff.parsers.universal.common.Tokenizer;
 import refdiff.parsers.universal.common.SourceFileReader;
 import refdiff.core.cst.CstNode;
-import refdiff.core.cst.CstNodeRelationship;
-import refdiff.core.cst.CstNodeRelationshipType;
 import refdiff.core.cst.CstRoot;
 import refdiff.core.cst.Location;
 import refdiff.core.cst.Stereotype;
 import refdiff.core.cst.TokenizedSource;
 import refdiff.parsers.universal.common.CallGraphGenerator;
+import refdiff.parsers.universal.common.InheritanceTreeGenerator;
 
 
 public class JavaParser {
@@ -35,37 +34,34 @@ public class JavaParser {
 
   public CstRoot parse(SourceFileSet folder) {
     TSParser parser = new TSParser();
-    TSLanguage tsLang = new TreeSitterJava();
+    TSLanguage tsLang = new TreeSitterJava(); // Keep tsLang instance for reuse
     parser.setLanguage(tsLang);
 
     CstRoot root = new CstRoot();
     Map<String, String> sourceCodeMap = SourceFileReader.readAllSourceFiles(folder);
 
-    for (Map.Entry<String, String> entry : sourceCodeMap.entrySet()) {
-      String filePath = entry.getKey();
-      String sourceCode = entry.getValue();
-      TSTree tree = parser.parseString(null, sourceCode);
-      addNodes(tree, tsLang, root, filePath, sourceCode);
+    // Parse all files once and store TSTrees
+    Map<String, TSTree> parsedTreeMap = new HashMap<>();
+    for (Map.Entry<String, String> sourceEntry : sourceCodeMap.entrySet()) {
+      TSTree tree = parser.parseString(null, sourceEntry.getValue());
+      parsedTreeMap.put(sourceEntry.getKey(), tree);
+    }
 
+    for (Map.Entry<String, TSTree> treeEntry : parsedTreeMap.entrySet()) {
+      String filePath = treeEntry.getKey();
+      TSTree tree = treeEntry.getValue();
+      String sourceCode = sourceCodeMap.get(filePath); // Get source code for context
+      addNodes(tree, tsLang, root, filePath, sourceCode);
+      
       TokenizedSource tokenizedSource = Tokenizer.tokenize(tree, tsLang, filePath, sourceCode); // TODO: Should the argument for tokenize be a relative path?
       root.addTokenizedFile(tokenizedSource);
     }
 
-    /* Create Hierarchy Graph */
-    List<CstNode> classOrInterfaceNodes = new ArrayList<>();
-    for (CstNode node : root.getNodes()) {
-      classOrInterfaceNodes.addAll(getClassOrInterfaceNodes(node));
-    }
-    Map<String, CstNode> classOrInterfaceNodeMap = new HashMap<>();
-    for (CstNode classOrInterfaceNode : classOrInterfaceNodes) {
-      classOrInterfaceNodeMap.put(classOrInterfaceNode.getSimpleName(), classOrInterfaceNode); //TODO simplenameをhashmapに持たせてるので重複しやすく上書きされる。namespaceなどを使うといいかも
-    }
-    for (Map.Entry<String, String> entry : sourceCodeMap.entrySet()) {
-      String sourceCode = entry.getValue();
-      TSTree tree = parser.parseString(null, sourceCode); // TODO parse済みのtreeを使いたい
-      addInheritanceRelationship(root, classOrInterfaceNodeMap, tree, tsLang, sourceCode);
-    }
-
+    List<String> inheritanceRelatedNodeTypes = Arrays.asList(JavaNodeTypes.CLASS_DECLARATION, JavaNodeTypes.INTERFACE_DECLARATION);
+    String inheritanceQuery = "[(superclass) (super_interfaces) (extends_interfaces)] @node";
+    InheritanceTreeGenerator inheritanceTreeGenerator = new InheritanceTreeGenerator(inheritanceRelatedNodeTypes, inheritanceQuery);
+    inheritanceTreeGenerator.buildInheritanceTree(root, sourceCodeMap, parsedTreeMap, tsLang);
+    
     CallGraphGenerator callGraphGenerator = new CallGraphGenerator(JavaNodeTypes.METHOD_DECLARATION);
     callGraphGenerator.generateCallGraph(root, sourceCodeMap);
 
@@ -189,83 +185,6 @@ public class JavaParser {
         }
       }
     }
-  }
-
-  private void addInheritanceRelationship(CstRoot root, Map<String, CstNode> classOrInterfaceNodeMap, TSTree tree, TSLanguage tsLang, String sourceCode) {
-    String query = "[(superclass) (super_interfaces) (extends_interfaces)] @node";
-    TSQuery tsQuery = new TSQuery(tsLang, query);
-    TSQueryCursor cursor = new TSQueryCursor();
-    TSNode rootNode = tree.getRootNode();
-    cursor.exec(tsQuery, rootNode);
-    TSQueryMatch match = new TSQueryMatch();
-
-    while (cursor.nextMatch(match)) {
-      TSQueryCapture[] captures = match.getCaptures();
-      for (TSQueryCapture capture : captures) {
-        TSNode tsNode = capture.getNode();
-        switch (tsNode.getType()) {
-          case "superclass": {
-            TSNode superclass = tsNode; // (superclass (type_identifier)) は extends Bar の2つを含む
-            TSNode extendsToken = superclass.getChild(0); // type_identifier は extends のこと
-            String superclassName = sourceCode.substring(extendsToken.getEndByte(), superclass.getEndByte()).trim(); // 先頭の空白を削除
-    
-            TSNode identifier = superclass.getParent().getChildByFieldName("name");
-            String className = sourceCode.substring(identifier.getStartByte(), identifier.getEndByte());
-    
-            if (!classOrInterfaceNodeMap.containsKey(superclassName)) { // 入力として与えられたフォルダにsuperclassの定義ファイルが含まれていなかった場合
-              continue;
-            }
-            root.getRelationships().add(new CstNodeRelationship(CstNodeRelationshipType.SUBTYPE, classOrInterfaceNodeMap.get(className).getId(), classOrInterfaceNodeMap.get(superclassName).getId()));
-            break; }
-          case "super_interfaces": { // class implements interfaces
-            TSNode interfaces = tsNode;
-            TSNode subClass = interfaces.getParent().getChildByFieldName("name");
-            String subClassName = sourceCode.substring(subClass.getStartByte(), subClass.getEndByte());
-
-            TSNode implementsToken = interfaces.getChild(0); // (super_interfaces (type_identifier)) type_identifier は implements のこと
-            List<String> interfaceNames = java.util.Arrays.stream(
-              sourceCode.substring(implementsToken.getEndByte(), interfaces.getEndByte()).split(",")).map(String::trim).toList(); // "Foo, Bar, Baz" -> ["Foo", "Bar", "Baz"]
-            for (String name : interfaceNames) {
-              if (!classOrInterfaceNodeMap.containsKey(name)) { // 入力として与えられたフォルダにinterfaceの定義ファイルが含まれていなかった場合
-                continue;
-              }
-              root.getRelationships().add(new CstNodeRelationship(CstNodeRelationshipType.SUBTYPE, classOrInterfaceNodeMap.get(subClassName).getId(), classOrInterfaceNodeMap.get(name).getId()));
-            }
-            break; }
-          case "extends_interfaces": { // interface extends interfaces
-            TSNode extendsInterfaces = tsNode;
-            TSNode subInterface = extendsInterfaces.getParent().getChildByFieldName("name");
-            String subInterfaceName = sourceCode.substring(subInterface.getStartByte(), subInterface.getEndByte());
-
-            TSNode extendsToken = extendsInterfaces.getChild(0); // (extends_interfaces (type_identifier)) type_identifier は extends のこと
-            List<String> extendedNames = java.util.Arrays.stream(
-              sourceCode.substring(extendsToken.getEndByte(), extendsInterfaces.getEndByte()).split(",")).map(String::trim).toList(); // "Foo, Bar, Baz" -> ["Foo", "Bar", "Baz"]
-            
-            for (String name : extendedNames) {
-              if (!classOrInterfaceNodeMap.containsKey(name)) { // 入力として与えられたフォルダにinterfaceの定義ファイルが含まれていなかった場合
-                continue;
-              }
-              root.getRelationships().add(new CstNodeRelationship(CstNodeRelationshipType.SUBTYPE, classOrInterfaceNodeMap.get(subInterfaceName).getId(), classOrInterfaceNodeMap.get(name).getId()));
-            }
-            break; }
-          default:
-            break;
-        }
-      }
-    }
-    return;
-  }
-
-  private List<CstNode> getClassOrInterfaceNodes(CstNode node) { // TODO CstRoot.forEachNode()で取れるかも
-    List<CstNode> classOrInterfaceNodes = new ArrayList<>();
-    for (CstNode child : node.getNodes()) {
-      classOrInterfaceNodes.addAll(getClassOrInterfaceNodes(child));
-    }
-    String nodeType = node.getType();
-    if (nodeType.equals(JavaNodeTypes.CLASS_DECLARATION) || nodeType.equals(JavaNodeTypes.INTERFACE_DECLARATION)) {
-      classOrInterfaceNodes.add(node);
-    }
-    return classOrInterfaceNodes;
   }
 
 }
