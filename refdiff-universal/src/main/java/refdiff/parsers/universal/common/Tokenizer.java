ac29489d33ea6de07f811336f2e12717a1318609
Ikuya Nagaki <ikuya313198@gmail.com>
2025-11-10 22:24:34 +0900

    refactor: tokenizer

--- refdiff-universal/src/main/java/refdiff/parsers/universal/common/Tokenizer.java
+++ refdiff-universal/src/main/java/refdiff/parsers/universal/common/Tokenizer.java
@@ -2,10 +2,6 @@ package refdiff.parsers.universal.common;
 
 import org.treesitter.TSLanguage;
 import org.treesitter.TSNode;
-import org.treesitter.TSQuery;
-import org.treesitter.TSQueryCapture;
-import org.treesitter.TSQueryCursor;
-import org.treesitter.TSQueryMatch;
 import org.treesitter.TSTree;
 import refdiff.core.cst.TokenPosition;
 import refdiff.core.cst.TokenizedSource;
@@ -16,25 +12,21 @@ import java.util.List;
 public class Tokenizer {
 
     public static TokenizedSource tokenize(TSTree tree, TSLanguage tsLang, String path) {
-        String query = "_ @node";
-        TSQuery tsQuery = new TSQuery(tsLang, query);
-        TSQueryCursor cursor = new TSQueryCursor();
         TSNode rootNode = tree.getRootNode();
-        cursor.exec(tsQuery, rootNode);
-        TSQueryMatch match = new TSQueryMatch();
-
         List<TokenPosition> tokens = new ArrayList<>();
-        while (cursor.nextMatch(match)) {
-            TSQueryCapture[] captures = match.getCaptures();
-            for (TSQueryCapture capture : captures) {
-                TSNode node = capture.getNode();
-                if (node.getChildCount() == 0) { // Leaf node
-                    int start = node.getStartByte();
-                    int end = node.getEndByte();
-                    tokens.add(new TokenPosition(start, end));
-                }
+        
+        collectLeafNodes(rootNode, tokens);
+        
+        return new TokenizedSource(path, tokens);
+    }
+
+    private static void collectLeafNodes(TSNode node, List<TokenPosition> tokens) {
+        if (node.getChildCount() == 0) {
+            tokens.add(new TokenPosition(node.getStartByte(), node.getEndByte()));
+        } else {
+            for (int i = 0; i < node.getChildCount(); i++) {
+                collectLeafNodes(node.getChild(i), tokens);
             }
         }
-        return new TokenizedSource(path, tokens);
     }
 }
