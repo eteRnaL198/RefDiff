08550c32e929082fc933faec179722e6bfce41c4
Ikuya Nagaki <ikuya313198@gmail.com>
2025-12-15 22:17:22 +0900

    refactor go plugin

--- refdiff-universal/src/main/java/refdiff/parsers/universal/go/GoNodeTypes.java
+++ refdiff-universal/src/main/java/refdiff/parsers/universal/go/GoNodeTypes.java
@@ -1,6 +1,7 @@
 package refdiff.parsers.universal.go;
 
 public class GoNodeTypes {
+  public static final String FILE = "file";
   public static final String FUNCTION = "function_declaration";
   public static final String METHOD = "method_declaration";
 }
--- refdiff-universal/src/main/java/refdiff/parsers/universal/go/GoParser.java
+++ refdiff-universal/src/main/java/refdiff/parsers/universal/go/GoParser.java
@@ -1,192 +1,177 @@
 package refdiff.parsers.universal.go;
 
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import org.treesitter.TSLanguage;
 import org.treesitter.TSNode;
-import org.treesitter.TSParser;
 import org.treesitter.TSQuery;
 import org.treesitter.TSQueryCapture;
 import org.treesitter.TSQueryCursor;
 import org.treesitter.TSQueryMatch;
 import org.treesitter.TSTree;
 import org.treesitter.TreeSitterGo;
-
 import refdiff.core.cst.CstNode;
-import refdiff.core.cst.CstRoot;
-import refdiff.core.cst.Location;
 import refdiff.core.cst.Parameter;
-import refdiff.core.cst.TokenizedSource;
 import refdiff.core.io.FilePathFilter;
-import refdiff.core.io.SourceFileSet;
-import refdiff.parsers.LanguagePlugin;
-import refdiff.parsers.universal.common.CallGraphGenerator;
-import refdiff.parsers.universal.common.SourceFileReader;
+import refdiff.parsers.universal.base.BasePlugin;
 import refdiff.parsers.universal.common.NodeUtils;
-import refdiff.parsers.universal.common.Tokenizer;
 
-import java.util.ArrayList;
-
-public class GoParser implements LanguagePlugin {
+public class GoParser extends BasePlugin {
   private int cstId = 0;
 
+  @Override
   public FilePathFilter getAllowedFilesFilter() {
     return new FilePathFilter(List.of(".go"));
   }
 
-  public CstRoot parse(SourceFileSet folder) {
-    TSParser parser = new TSParser();
-    TSLanguage tsLang = new TreeSitterGo();
-    parser.setLanguage(tsLang);
-
-    CstRoot root = new CstRoot();
-
-    Map<String, String> sourceCodeMap = SourceFileReader.readAllSourceFiles(folder);
-    Map<String, TSTree> parsedTreeMap = new HashMap<>();
-    for (Map.Entry<String, String> sourceEntry : sourceCodeMap.entrySet()) {
-      TSTree tree = parser.parseString(null, sourceEntry.getValue());
-      parsedTreeMap.put(sourceEntry.getKey(), tree);
-    }
-
-    for (Map.Entry<String, TSTree> treeEntry : parsedTreeMap.entrySet()) {
-      String filePath = treeEntry.getKey();
-      TSTree tree = treeEntry.getValue();
-      String sourceCode = sourceCodeMap.get(filePath);
-      byte[] sourceBytes = sourceCode.getBytes(StandardCharsets.UTF_8);
-      addNodes(tree, tsLang, root, filePath, sourceBytes);
-
-      TokenizedSource tokenizedSource = Tokenizer.tokenize(tree, tsLang, filePath);
-      root.addTokenizedFile(tokenizedSource);
-    }
-
-    CallGraphGenerator callGraphGenerator = new CallGraphGenerator(GoNodeTypes.METHOD, GoNodeTypes.FUNCTION);
-    callGraphGenerator.generateCallGraph(root, sourceCodeMap);
-
-    return root;
+  @Override
+  protected TSLanguage getLanguage() {
+    return new TreeSitterGo();
   }
 
-  private void addNodes(TSTree tree, TSLanguage tsLang, CstRoot cstRoot, String filePath, byte[] sourceCode) {
-    String packageQuerySrc = "(package_clause (package_identifier) @package_name)";
-    TSQuery packageQuery = new TSQuery(tsLang, packageQuerySrc);
-    TSQueryCursor packageCursor = new TSQueryCursor();
-    packageCursor.exec(packageQuery, tree.getRootNode());
-    String namespace = "main"; 
-    TSQueryMatch packageMatch = new TSQueryMatch();
-    if (packageCursor.nextMatch(packageMatch)) {
-        for (TSQueryCapture capture : packageMatch.getCaptures()) {
-            TSNode capturedNode = capture.getNode();
-            namespace = NodeUtils.getNodeText(capturedNode, sourceCode);
-            break;
-        }
-    }
+  @Override
+  protected String[] getCallableNodeTypes() {
+    return new String[] { GoNodeTypes.METHOD, GoNodeTypes.FUNCTION };
+  }
 
-    String querySrc = "[ " +
-        "(function_declaration name: (identifier) @name parameters: (parameter_list) @params body: (block) @body) @function" +
-        " (method_declaration receiver: (parameter_list) @receiver name: (field_identifier) @name parameters: (parameter_list) @params body: (block) @body) @method" +
-        " (function_declaration name: (identifier) @name parameters: (parameter_list) @params result: (_) @result body: (block) @body) @function" +
-        " (method_declaration receiver: (parameter_list) @receiver name: (field_identifier) @name parameters: (parameter_list) @params result: (_) @result body: (block) @body) @method" +
-    "]";
+  @Override
+  protected String[] getInheritableNodeTypes() {
+    return new String[] {};
+  }
+
+  @Override
+  protected void buildCst(TSTree tree, TSLanguage tsLang, String filePath, byte[] sourceCode) {
+    String namespace = extractNamespace(tree, tsLang, sourceCode);
+
+    String querySrc = """
+    [
+        (function_declaration
+            name: (identifier) @name
+            parameters: (parameter_list) @params
+            body: (block) @body) @function
+        (method_declaration
+            receiver: (parameter_list) @receiver
+            name: (field_identifier) @name
+            parameters: (parameter_list) @params
+            body: (block) @body) @method
+        (function_declaration
+            name: (identifier) @name
+            parameters: (parameter_list) @params
+            result: (_) @result
+            body: (block) @body) @function
+        (method_declaration
+            receiver: (parameter_list) @receiver
+            name: (field_identifier) @name
+            parameters: (parameter_list) @params
+            result: (_) @result
+            body: (block) @body) @method
+    ]
+    """;
 
     TSQuery query = new TSQuery(tsLang, querySrc);
     TSQueryCursor cursor = new TSQueryCursor();
     cursor.exec(query, tree.getRootNode());
 
     TSQueryMatch match = new TSQueryMatch();
-    while(cursor.nextMatch(match)) {
-        TSNode nameNode = null;
-        TSNode paramsNode = null;
-        TSNode bodyNode = null;
-        TSNode defNode = null;
-        String nodeType = GoNodeTypes.FUNCTION;
+    while (cursor.nextMatch(match)) {
+      TSNode nameNode = null;
+      TSNode paramsNode = null;
+      TSNode bodyNode = null;
+      TSNode declaration = null;
+      String nodeType = null;
 
-        for (TSQueryCapture capture : match.getCaptures()) {
-            TSNode capturedNode = capture.getNode();
-            String captureName = query.getCaptureNameForId(capture.getIndex());
+      for (TSQueryCapture capture : match.getCaptures()) {
+        TSNode capturedNode = capture.getNode();
+        String captureName = query.getCaptureNameForId(capture.getIndex());
 
-            switch (captureName) {
-                case "function":
-                    defNode = capturedNode;
-                    nodeType = GoNodeTypes.FUNCTION;
-                    break;
-                case "method":
-                    defNode = capturedNode;
-                    nodeType = GoNodeTypes.METHOD;
-                    break;
-                case "name":
-                    nameNode = capturedNode;
-                    break;
-                case "params":
-                    paramsNode = capturedNode;
-                    break;
-                case "body":
-                    bodyNode = capturedNode;
-                    break;
-            }
+        switch (captureName) {
+            case "function":
+                declaration = capturedNode;
+                nodeType = GoNodeTypes.FUNCTION;
+            break;
+            case "method":
+                declaration = capturedNode;
+                nodeType = GoNodeTypes.METHOD;
+            break;
+            case "name":
+                nameNode = capturedNode;
+                break;
+            case "params":
+                paramsNode = capturedNode;
+                break;
+            case "body":
+                bodyNode = capturedNode;
+                break;
         }
+      }
 
-        if (defNode != null && nameNode != null && bodyNode != null) {
-            CstNode cstNode = new CstNode(cstId++);
-            cstNode.setType(nodeType);
-            
-            String simpleName = NodeUtils.getNodeText(nameNode, sourceCode);
-            cstNode.setSimpleName(simpleName);
-            cstNode.setNamespace(namespace + ".");
+      if (declaration != null && nameNode != null && bodyNode != null) {
+        CstNode cstNode = new CstNode(cstId++);
+        cstNode.setType(nodeType);
+        
+        String simpleName = NodeUtils.getNodeText(nameNode, sourceCode);
+        cstNode.setSimpleName(simpleName);
+        cstNode.setNamespace(namespace + ".");
+        
+        cstNode.setLocation(NodeUtils.generateLocation(declaration, bodyNode, filePath));
 
-            int startByte = defNode.getStartByte();
-            int endByte = defNode.getEndByte();
-            int bodyStartByte = bodyNode.getStartByte();
-            int bodyEndByte = bodyNode.getEndByte();
-            int startLine = defNode.getStartPoint().getRow() + 1;
-            int endLine = defNode.getEndPoint().getRow() + 1;
-            cstNode.setLocation(new Location(filePath, startByte, endByte, startLine, endLine, bodyStartByte, bodyEndByte));
-
-            String paramsString = "()";
-            if (paramsNode != null) {
-                paramsString = NodeUtils.getNodeText(paramsNode, sourceCode);
-            }
-            cstNode.setLocalName(simpleName + paramsString);
-            cstNode.setParameters(extractParameters(paramsNode, sourceCode));
-            
-            cstRoot.addNode(cstNode);
+        String paramsString = "()";
+        if (paramsNode != null) {
+          paramsString = NodeUtils.getNodeText(paramsNode, sourceCode);
         }
+        cstNode.setLocalName(simpleName + paramsString);
+        cstNode.setParameters(extractParameters(paramsNode, sourceCode));
+
+        addNodeToParent(cstNode, namespace);
+      }
     }
   }
 
+  private String extractNamespace(TSTree tree, TSLanguage tsLang, byte[] sourceCode) {
+    String querySrc = "(package_clause (package_identifier) @package_name)";
+    TSQuery query = new TSQuery(tsLang, querySrc);
+    TSQueryCursor cursor = new TSQueryCursor();
+    cursor.exec(query, tree.getRootNode());
+
+    TSQueryMatch match = new TSQueryMatch();
+    if (cursor.nextMatch(match)) {
+      for (TSQueryCapture capture : match.getCaptures()) {
+        TSNode capturedNode = capture.getNode();
+        return NodeUtils.getNodeText(capturedNode, sourceCode);
+      }
+    }
+    return "";
+  }
+
   private List<Parameter> extractParameters(TSNode paramsNode, byte[] sourceCode) {
-      List<Parameter> parameters = new ArrayList<>();
-      if (paramsNode == null) {
-          return parameters;
-      }
-
-      for (int i = 0; i < paramsNode.getNamedChildCount(); i++) {
-          TSNode paramDecl = paramsNode.getNamedChild(i);
-          String nodeType = paramDecl.getType();
-
-          if (nodeType.equals("parameter_declaration")) {
-              int namedChildCount = paramDecl.getNamedChildCount();
-              if (namedChildCount > 0) {
-                  for (int j = 0; j < namedChildCount - 1; j++) {
-                      TSNode nameNode = paramDecl.getNamedChild(j);
-                      if (nameNode.getType().equals("identifier")) {
-                          String paramName = NodeUtils.getNodeText(nameNode, sourceCode);
-                          parameters.add(new Parameter(paramName));
-                      }
-                  }
-              }
-          } else if (nodeType.equals("variadic_parameter_declaration")) {
-              if (paramDecl.getNamedChildCount() > 0) {
-                  TSNode nameNode = paramDecl.getNamedChild(0);
-                  String paramName = NodeUtils.getNodeText(nameNode, sourceCode);
-                  parameters.add(new Parameter(paramName));
-              }
-          }
-      }
+    List<Parameter> parameters = new ArrayList<>();
+    if (paramsNode == null) {
       return parameters;
+    }
+
+    for (int i = 0; i < paramsNode.getNamedChildCount(); i++) {
+      TSNode paramDecl = paramsNode.getNamedChild(i);
+      String nodeType = paramDecl.getType();
+
+      if (nodeType.equals("parameter_declaration")) {
+        int namedChildCount = paramDecl.getNamedChildCount();
+        if (namedChildCount > 0) {
+          for (int j = 0; j < namedChildCount - 1; j++) {
+            TSNode nameNode = paramDecl.getNamedChild(j);
+            if (nameNode.getType().equals("identifier")) {
+              String paramName = NodeUtils.getNodeText(nameNode, sourceCode);
+              parameters.add(new Parameter(paramName));
+            }
+          }
+        }
+      } else if (nodeType.equals("variadic_parameter_declaration")) {
+        if (paramDecl.getNamedChildCount() > 0) {
+          TSNode nameNode = paramDecl.getNamedChild(0);
+          String paramName = NodeUtils.getNodeText(nameNode, sourceCode);
+          parameters.add(new Parameter(paramName));
+        }
+      }
+    }
+    return parameters;
   }
 }
