c845986311c70ba9a2de8da207cf47c787603c8b
Ikuya Nagaki <ikuya313198@gmail.com>
2026-01-25 01:04:38 +0900

    fix location when body is null

--- refdiff-universal/src/main/java/refdiff/parsers/universal/common/NodeUtils.java
+++ refdiff-universal/src/main/java/refdiff/parsers/universal/common/NodeUtils.java
@@ -22,9 +22,9 @@ public class NodeUtils {
             decl.getStartByte(),
             decl.getEndByte(),
             decl.getStartPoint().getRow() + 1,
-            body != null ? body.getStartPoint().getRow() + 1 : decl.getStartPoint().getRow() + 1,
+            body != null ? body.getStartPoint().getRow() + 1 : decl.getEndPoint().getRow() + 1,
             body != null ? body.getEndPoint().getRow() + 1 : decl.getEndPoint().getRow() + 1,
-            body != null ? body.getStartByte() : decl.getStartByte(),
+            body != null ? body.getStartByte() : decl.getEndByte(),
             body != null ? body.getEndByte() : decl.getEndByte()
         );
     }
