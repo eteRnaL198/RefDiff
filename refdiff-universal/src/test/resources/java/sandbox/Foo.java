59e72bd26407f3ffd5f2dd4f43492753e8cb611a
Ikuya Nagaki <ikuya313198@gmail.com>
2026-02-25 07:20:40 +0900

    add sandbox

---
+++ refdiff-universal/src/test/resources/java/sandbox/Foo.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Foo extends Bar {
+  public static void main(String[] args) {
+    print();
+  }
+
+  public static void print() {
+    System.out.println("Hello,World");
+  }
+}
